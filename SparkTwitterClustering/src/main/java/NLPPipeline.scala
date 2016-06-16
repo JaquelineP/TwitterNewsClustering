933f552710fde582065596cc52dd157bf40d07ce
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2016-06-16 16:27:43 +0200

    adjusted sanitizer implementation to subclass StopWordsRemover

--- SparkTwitterClustering/src/main/java/NLPPipeline.scala
+++ SparkTwitterClustering/src/main/java/NLPPipeline.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.{IDF, HashingTF, StopWordsRemover, Tokenizer, TweetSanitizer}
+import org.apache.spark.ml.feature.{IDF, HashingTF, StopWordsRemover, Tokenizer}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.mllib.linalg._
--- SparkTwitterClustering/src/main/java/TweetSanitizer.scala
+++ SparkTwitterClustering/src/main/java/TweetSanitizer.scala
@@ -1,24 +1,8 @@
-package org.apache.spark.ml.feature
-
-import org.apache.spark.annotation.{Since, Experimental}
-import org.apache.spark.ml.Transformer
-import org.apache.spark.ml.param.{BooleanParam, ParamMap, StringArrayParam}
-import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
-import org.apache.spark.ml.util._
+import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, udf}
-import org.apache.spark.sql.types.{ArrayType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions._
 
-class TweetSanitizer(override val uid: String)
-  extends Transformer with HasInputCol with HasOutputCol with DefaultParamsWritable {
-
-  def this() = this(Identifiable.randomUID("tweetSanitizer"))
-
-  /** @group setParam */
-  def setInputCol(value: String): this.type = set(inputCol, value)
-
-  /** @group setParam */
-  def setOutputCol(value: String): this.type = set(outputCol, value)
+class TweetSanitizer extends StopWordsRemover {
 
   override def transform(dataset: DataFrame): DataFrame = {
     val outputSchema = transformSchema(dataset.schema)
@@ -35,17 +19,4 @@ class TweetSanitizer(override val uid: String)
     dataset.select(col("*"), t(col($(inputCol))).as($(outputCol), metadata))
   }
 
-  override def transformSchema(schema: StructType): StructType = {
-    val inputType = schema($(inputCol)).dataType
-    require(inputType.sameType(ArrayType(StringType)),
-      s"Input type must be ArrayType(StringType) but got $inputType.")
-    SchemaUtils.appendColumn(schema, $(outputCol), inputType, schema($(inputCol)).nullable)
-  }
-
-  override def copy(extra: ParamMap): TweetSanitizer = defaultCopy(extra)
-}
-
-object TweetSanitizer extends DefaultParamsReadable[TweetSanitizer] {
-
-  override def load(path: String): TweetSanitizer = super.load(path)
 }
