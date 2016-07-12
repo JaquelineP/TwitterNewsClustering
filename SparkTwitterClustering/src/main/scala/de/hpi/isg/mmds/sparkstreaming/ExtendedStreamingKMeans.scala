981499c06ce47580e629d69e54d1d24af272b2fe
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2016-07-12 11:56:54 +0200

    extended trainOn to generate new clusters for tweets that are >100 away from best matching centroid

--- SparkTwitterClustering/src/main/scala/de/hpi/isg/mmds/sparkstreaming/ExtendedStreamingKMeans.scala
+++ SparkTwitterClustering/src/main/scala/de/hpi/isg/mmds/sparkstreaming/ExtendedStreamingKMeans.scala
@@ -1,16 +1,21 @@
 package de.hpi.isg.mmds.sparkstreaming
 
 import org.apache.spark.mllib.clustering.{StreamingKMeans, StreamingKMeansModel}
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.streaming.dstream.DStream
 
 class ExtendedStreamingKMeans extends StreamingKMeans {
+
+  private def updateModel(newCenters: Array[Vector], newWeights: Array[Double]): this.type = {
+    model = new StreamingKMeansModel(newCenters, newWeights)
+    setK(newCenters.length)
+    this
+  }
 
   def addCentroids(centers: Array[Vector], weights: Array[Double]): this.type = {
     val newCenters = model.clusterCenters ++ centers
     val newWeights = model.clusterWeights ++ weights
-    model = new StreamingKMeansModel(newCenters, newWeights)
-    setK(newCenters.length)
-    this
+    this.updateModel(newCenters, newWeights)
   }
 
   def removeCentroids(centers: Array[Int]): this.type = {
@@ -22,9 +27,25 @@ class ExtendedStreamingKMeans extends StreamingKMeans {
       .zipWithIndex
       .filter { case (weight, index) => !centers.contains(index) }
       .map { case (weight, index) => weight }
-    model = new StreamingKMeansModel(newCenters, newWeights)
-    setK(newCenters.length)
-    this
+    updateModel(newCenters, newWeights)
+  }
+
+  override def trainOn(data: DStream[Vector]) {
+    data.foreachRDD { (rdd) =>
+      val distantTweets = rdd.filter { (vector) =>
+        val center = model.predict(vector)
+        Vectors.sqdist(vector, latestModel().clusterCenters(center)) > 100
+      }.collect()
+
+      var newCenters = Array[Vector]()
+      distantTweets.foreach { tweet =>
+        var addNewCenter = true
+        newCenters.foreach(center => addNewCenter &&= Vectors.sqdist(tweet, center) > 50)
+        if (addNewCenter) newCenters +:= tweet
+      }
+      addCentroids(newCenters, Array.fill[Double](newCenters.length)(1.0))
+    }
+    super.trainOn(data)
   }
 
 }
