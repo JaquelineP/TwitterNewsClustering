6f952e590053b1dff6a68af8d100a9d73706a126
Jaqueline Pollak <jaqueline.pollak@student.hpi.de>
2016-07-14 16:36:27 +0200

    convert data to new 'webinterface'

--- webapp/server.js
+++ webapp/server.js
@@ -7,154 +7,87 @@ var app = express();
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
+function readData() {
 
-// read data and create "json object"
-var data  = {};
-var fs = require('fs');
-// get filenames
-var path = '../SparkTwitterClustering/output/merged_clusterInfo'
-var filenames = fs.readdirSync(path);
-filenames = filenames.filter(function(e){
-    // startwith
-    return e.lastIndexOf('part-', 0) === 0
-});
-var lines = []
-
-// for each filename
-var batch = [];
-var k = -1
-var pre = -1
-for (index in filenames){
-    var filename = path + '/' + filenames[index]
-    var array = fs.readFileSync(filename).toString().split(")\n");
-    if (array.length == 1) {
-        console.log("empty file "+ filenames[index])
-        continue;
-    }
-    for(i in array) {
-        var line = array[i];
-        // remove first char (
-        line = line.substring(1);
-        var lineObj = line.split(",")
-        if (lineObj.length < 8) continue;
-        lines.push(lineObj);
-        var time = parseInt(lineObj[8])
-        if (! (time in data)) {
-            data[time] = []
-        }
-        data[time].push(lineObj)
-    }
-}
-
-var dataArray = []
-Object.keys(data).forEach(function (key) {
-    console.log(key)
-    var empty = [pre + 1, 0, -1, 0, 0, '000000000000000000|||||', '', false, key]
-
-    // add empty clusters
-    var pre = -1
-    var array = []
-    data[key].forEach(function(line){
-        var clusterId  = parseInt(line[0]);
-        while (pre + 1 < clusterId) {
-            array.push(empty);
-            pre++;
-        }
-        array.push(line)
-        pre++;
+    // read data and create "json object"
+    var data  = {};
+    var fs = require('fs');
+    // get filenames
+    var path = '../SparkTwitterClustering/output/merged_clusterInfo'
+    var filenames = fs.readdirSync(path);
+    filenames = filenames.filter(function(e){
+        // startwith
+        return e.lastIndexOf('part-', 0) === 0
     });
-    dataArray.push(array);
-});
+    var lines = []
+
+    // for each filename
+    for (index in filenames){
+        var filename = path + '/' + filenames[index]
+        var array = fs.readFileSync(filename).toString().split(")\n");
+        if (array.length == 1) {
+            console.log("empty file "+ filenames[index])
+            continue;
+        }
+        array.forEach(function(line) {
+
+            // remove first char (
+            line = line.substring(1);
+            var lineArray = line.split(",")
+            if (lineArray.length < 8) return;
+            var parsedLine = {
+                'id': lineArray[0],
+                'count': lineArray[1],
+                'tweet': 'https://twitter.com/nytimes/status/' + lineArray[5],
+                'newsUrl': lineArray[6],
+                'silhouette': lineArray[2],
+                'intra': lineArray[3],
+                'inter': lineArray[4],
+                'interesting' : lineArray[7] == 'true',
+                'batch_time' : lineArray[8]
+            }
+            lines.push(parsedLine);
+        });
+    }
+
+    // group lines by clusterid
+    function groupBy( array , f ) {
+      var groups = {};
+      array.forEach( function( o )
+      {
+        var group = JSON.stringify( f(o) );
+        groups[group] = groups[group] || [];
+        groups[group].push( o );
+      });
+      return Object.keys(groups).map( function( group )
+      {
+        return groups[group];
+      })
+    }
+
+    var result = groupBy(lines, function(item) {
+      return [item.id];
+    });
+
+    // filter uninteresting clusters
+    return result.filter(function(cluster) {
+        var interesting = false;
+        cluster.forEach(function(line){
+            interesting = interesting || line.interesting
+        });
+        return interesting;
+    })
+}
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-var interesting = [];
-for (i = 0; i < dataArray.length; i++) {
-    for (y = 0; y < dataArray[i].length; y++) {
-        if (dataArray[i][y][7] == 'true')
-            if (!(y in interesting)) interesting.push(y);
-    }
-}
-
 app.get('/index', function(req, res) {
-        var clusters = [
-        [
-            {
-                'id': 1,
-                'count': 10,
-                'tweet': 'https://twitter.com/nytimes/status/753559763808612352',
-                'newsUrl': 'http://www.nytimes.com/2016/07/15/us/politics/house-democrats-campaign.html?smid=tw-nytimes&smtyp=cur&_r=0',
-                'silhouette': 1.2,
-                'intra': 0.3,
-                'inter': 0.1
-            },
-            {
-                'id': 1,
-                'count': 15,
-                'tweet': 'https://twitter.com/nytimes/status/753559763808612352',
-                'newsUrl': 'http://www.nytimes.com/2016/07/15/us/politics/house-democrats-campaign.html?smid=tw-nytimes&smtyp=cur&_r=0',
-                'silhouette': 1.2,
-                'intra': 0.3,
-                'inter': 0.1
-            },
-            {
-                'id': 1,
-                'count': 20,
-                'tweet': 'https://twitter.com/nytimes/status/753559763808612352',
-                'newsUrl': 'http://www.nytimes.com/2016/07/15/us/politics/house-democrats-campaign.html?smid=tw-nytimes&smtyp=cur&_r=0',
-                'silhouette': 1.2,
-                'intra': 0.3,
-                'inter': 0.1
-            },
-
-        ],
-        [
-            {
-                'id': 2,
-                'count': 40,
-                'tweet': 'https://twitter.com/nytimes/status/753559763808612352',
-                'newsUrl': 'http://www.nytimes.com/2016/07/15/us/politics/house-democrats-campaign.html?smid=tw-nytimes&smtyp=cur&_r=0',
-                'silhouette': 1.2,
-                'intra': 0.3,
-                'inter': 0.1
-            },
-            {
-                'id': 2,
-                'count': 20,
-                'tweet': 'https://twitter.com/nytimes/status/753559763808612352', 
-                'newsUrl': 'http://www.nytimes.com/2016/07/15/us/politics/house-democrats-campaign.html?smid=tw-nytimes&smtyp=cur&_r=0',
-                'silhouette': 1.2,
-                'intra': 0.3,
-                'inter': 0.1
-            },
-            {
-                'id': 2,
-                'count': 20,
-                'tweet': 'https://twitter.com/nytimes/status/753559763808612352', 
-                'newsUrl': 'http://www.nytimes.com/2016/07/15/us/politics/house-democrats-campaign.html?smid=tw-nytimes&smtyp=cur&_r=0',
-                'silhouette': 1.2,
-                'intra': 0.3,
-                'inter': 0.1
-            },
-        ]
-    ]
     res.render('index.html', {
         // enter params for view here
         title: "Twitter News Stream",
-        clusters: clusters,
-        interesting: interesting
+        clusters: readData()
     });
 });
 
-app.get('/', function(req, res) {
-
-    res.render('home', {
-        // enter params for view here
-        title: "Twitter News Stream",
-        clusters: dataArray,
-        interesting: interesting
-    });
-});
-  
 app.listen(3000);
