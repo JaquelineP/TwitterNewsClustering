7b9867ae5a7e81344964ddffb50d2c9d54e49440
Benjamin Reissaus <benjamin.reissaus@student.hpi.uni-potsdam.de>
2016-07-19 09:51:58 +0200

    adapt run and bootstrap script

--- SparkTwitterClustering/scripts/runOnCluster.sh
+++ SparkTwitterClustering/scripts/runOnCluster.sh
@@ -4,24 +4,27 @@ rm runtime.csv
 
 echo "Start deploying on server"
 
-# for loop from node 1 to 20
-for ((node = 12; node>=1; node=node-2))
+batchSizes=( 10000 20000 30000)
+
+for batchSize in "${batchSizes[@]}"
 do
+	for ((node = 12; node>=1; node=node-2))
+	do
 
-        # run each setting 3 times
-        for j in `seq 1 1`;
-        do
-                echo "Run with $node nodes "
-                echo "$node," >> runtime.csv
-                spark-submit --class de.hpi.isg.mmds.sparkstreaming.Main \
-                --driver-memory 4g --executor-memory 2g --total-executor-cores $node --num-executors $node \
-                target/SparkTwitterClustering-jar-with-dependencies.jar \
-                -input hdfs:///twitter.dat \
-                -tweetsPerBatch 10000 -maxBatchCount 10 -runtime  >> runtime.csv
-                echo "   " >> runtime.csv
-        done
+		# run each setting 3 times
+		for j in `seq 1 3`;
+		do
+			echo "Run with $node nodes and batch size $batchSize"
+			echo "$node," >> runtime.csv
+			spark-submit --class de.hpi.isg.mmds.sparkstreaming.Main \
+			--driver-memory 4g --executor-memory 2g --total-executor-cores $node --num-executors $node \
+			SparkTwitterClustering-jar-with-dependencies.jar \
+			-input hdfs:///twitter.dat \
+			-tweetsPerBatch $batchSize -maxBatchCount 10 -runtime  >> runtime.csv
+			echo "   " >> runtime.csv
+		done
+	done
 done
-
 # tear down
 # echo "remove file from hdfs"
 #/opt/hadoop/default/bin/hadoop fs -rm /data/mmds16/twitter/twitter.dat
